5b24616e6619a2bd2440eee6abc8219686cf3abe
skaza <skaza@towson.edu>
2012-10-09 16:22:57 -0400

    some changes

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,6 +4,7 @@ class LoginController < ApplicationController
 
   def login
     session[:logged_in]=1
+    #main controller index action
     redirect_to (url_for(:controller=>'main',:action=>'index'))
   end
 
